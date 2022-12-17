db95952182b24e598aeb2e569a8550101b129238
jabko123 <118082299+jabko123@users.noreply.github.com>
2022-12-17 01:08:06 +0000

    Create jump.lua

---
+++ jump.lua
@@ -0,0 +1,4 @@
+while task.wait() do
+game.Players.LocalPlayer.Character.Humanoid.Jump = true
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-6080, 7230.06836, -100, 1111, 1111, -0.995195925, 9.47870227e-09, 0.998104811, 0.0615372099, 0.997085631, 0.00469470629, -0.076146096)
+loop
